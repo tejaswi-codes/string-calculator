cdf23b65b29a6a372e467a37fc826aa99638e77d
tejaswi-codes <tejaswi.manipal@gmail.com>
2024-11-19 18:22:36 +0530

    refactored code to handle custom defined delimiter scenario

--- src/utils/stringCalculator.js
+++ src/utils/stringCalculator.js
@@ -1,10 +1,34 @@
-import { DEFAULT_DELIMITER, GLOBAL_DELIMITER } from "./data"
+import { CUSTOM_DELIMITER_INDICATOR, DEFAULT_DELIMITER, GLOBAL_DELIMITER } from "./data"
 
 
 export const add = (numbers) => {
+    // basic condition 1
     if (numbers === "") return 0
-    if (numbers.indexOf(DEFAULT_DELIMITER) === -1) return parseInt(numbers)
 
-    const chunks = numbers.split(new RegExp(`${DEFAULT_DELIMITER}|${GLOBAL_DELIMITER}`))
+
+    // init
+    let delimiter = DEFAULT_DELIMITER
+    let numStr = numbers
+
+
+    // re-assignment
+    if (numbers.startsWith(CUSTOM_DELIMITER_INDICATOR)) {
+        delimiter = numbers.charAt(CUSTOM_DELIMITER_INDICATOR.length)
+        numStr = getNumStrForCustomDelimiter(numbers)
+    }
+
+    // splitting 
+    const chunks = numStr.split(new RegExp(`${delimiter}|${GLOBAL_DELIMITER}`))
+
+    // adding
     return chunks.reduce((sum, num) => sum + parseInt(num), 0)
+}
+
+
+
+
+// Helper function
+const getNumStrForCustomDelimiter = (fullStr) => {
+    const firstLineEndsAt = fullStr.indexOf(GLOBAL_DELIMITER)
+    return fullStr.substring(firstLineEndsAt + 1)
 }
