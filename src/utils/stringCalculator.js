75c0913fde4251df2c7fda19fc28a090d82774d5
tejaswi-codes <tejaswi.manipal@gmail.com>
2024-11-19 16:50:53 +0530

    refactored code to handle comma-separated 2 numbers input

--- src/utils/stringCalculator.js
+++ src/utils/stringCalculator.js
@@ -1,4 +1,7 @@
 export const add = (numbers) => {
     if (numbers === "") return 0
-    return parseInt(numbers)
+    if (numbers.indexOf(",") === -1) return parseInt(numbers)
+
+    let chunks = numbers.split(",")    
+    return parseInt(chunks[0])+parseInt(chunks[1])
 }
