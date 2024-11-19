6348238e9dcf4f5075b3443ca56c91dd8ca281aa
tejaswi-codes <tejaswi.manipal@gmail.com>
2024-11-19 16:37:01 +0530

    added test case for returning the number itself when given a single number

--- src/tests/stringCalculator.test.js
+++ src/tests/stringCalculator.test.js
@@ -3,7 +3,13 @@ import { add } from '../utils/stringCalculator'
 
 
 describe('string calculator', () => {
+
     it("should return 0 given an empty string", () => {
         expect(add("")).toBe(0)
     })
+
+    it("should return the number itself given a single number", () => {
+        expect(add("1")).toBe(1)
+    })
+
 })
