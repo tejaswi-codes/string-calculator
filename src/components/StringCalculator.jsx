d1d8b65b490483f6376cb3191939d6bdfb2de588
tejaswi-codes <tejaswi.manipal@gmail.com>
2024-11-21 22:29:35 +0530

    added app-description

--- src/components/StringCalculator.jsx
+++ src/components/StringCalculator.jsx
@@ -26,6 +26,14 @@ const StringCalculator = () => {
 	return (
 		<article className='w-full h-[100vh] vflex !gap-10 bg-ashGray font-sans'>
 			<h1 className='text-3xl tracking-wider text-midnightBlue'>String Calculator</h1>
+			<p className='para'>
+				Simply enter a series of numbers separated by commas 
+				and click &lsquo;Calculate&rsquo; to get their sum.
+				To change the delimiter, declare the delimiter prefixed with 
+				<span className='inlineCode'>{`//`}</span> 
+				on the first line.
+				Try it out and get your sum instantly!
+			</p>
 			<textarea
 				className='w-96 h-32 p-4 deepShadow textareaScrollbar'
 				placeholder='Please, enter your input here.'
