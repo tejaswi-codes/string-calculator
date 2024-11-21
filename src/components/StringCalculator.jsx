ab2046f3b68833b1b941e85ae06ebb961e83b57f
tejaswi-codes <tejaswi.manipal@gmail.com>
2024-11-21 21:56:53 +0530

    added tailwind config and init StringCalculator component

---
+++ src/components/StringCalculator.jsx
@@ -0,0 +1,9 @@
+const StringCalculator = () => {
+	return (
+		<article className='w-full h-[100vh] vflex !gap-10 bg-ashGray font-sans'>
+			<h1 className='text-3xl tracking-wider text-midnightBlue'>String Calculator</h1>
+		</article>
+	)
+}
+
+export default StringCalculator
